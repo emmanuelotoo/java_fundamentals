d4b00c0f0065c2088c7216dba67b855431d8e5c9
Emmanuel <eotoo218@gmail.com>
2025-11-17 15:25:19 +0000

    add Rectangle class for revising objects and encapsulation

---
+++ src/main/java/dev/emmanuelotoo/objects/Rectangle.java
@@ -0,0 +1,40 @@
+package dev.emmanuelotoo.objects;
+
+public class Rectangle {
+
+    private double length;
+    private double width;
+    private int size = 4;
+
+    public double calculatePerimeter() {
+        return (2 * length) + (2 * width);
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
