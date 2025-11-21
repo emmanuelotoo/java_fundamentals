d0f1a1f927a3e57e6cd0634766e2265301ce1a9c
Emmanuel <eotoo218@gmail.com>
2025-11-21 13:09:04 +0000

    add Book and Product classes for revising interfaces

---
+++ src/main/java/dev/emmanuelotoo/interfaces/Library.java
@@ -0,0 +1,8 @@
+package dev.emmanuelotoo.interfaces;
+
+public class Library {
+    public static void main(String[] args) {
+        Product book = new Book();
+        book.setName("A tale of two clowns");
+    }
+}
---
+++ src/main/java/dev/emmanuelotoo/interfaces/Product.java
@@ -0,0 +1,14 @@
+package dev.emmanuelotoo.interfaces;
+
+public interface Product {
+    String getName();
+    void setName(String name);
+
+    default double getPrice() {
+        return 50;
+    }
+
+    default void setPrice(double price) {
+
+    }
+}
