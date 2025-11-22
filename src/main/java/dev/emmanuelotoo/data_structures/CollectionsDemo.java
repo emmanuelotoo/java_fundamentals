6ca8de44793e45d03eb4343c93515c31f931eef2
Emmanuel <eotoo218@gmail.com>
2025-11-22 11:52:18 +0000

    add listDemo method to CollectionsDemo class for revising Lists

--- src/main/java/dev/emmanuelotoo/data_structures/CollectionsDemo.java
+++ src/main/java/dev/emmanuelotoo/data_structures/CollectionsDemo.java
@@ -1,12 +1,15 @@
 package dev.emmanuelotoo.data_structures;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CollectionsDemo {
 
     public static void main(String[] args) {
-        setDemo();
+//        setDemo();
+        listDemo();
     }
     public static void setDemo() {
         Set fruits = new HashSet();
@@ -24,5 +27,21 @@ public class CollectionsDemo {
         Set moreFruit = Set.of("pear", "raisin", "cherry");
         System.out.println(moreFruit);
 
+    }
+    public static void listDemo() {
+        List fruits = new ArrayList();
+        fruits.add("apple");
+        fruits.add("lemon");
+        fruits.add("pear");
+
+        fruits.set(1, "grape");
+        fruits.add("lemon");
+        fruits.remove("pear"); // removes first instance of object found
+        System.out.println("index 1: " + fruits.get(1));
+        System.out.println(fruits.indexOf("grape"));
+        System.out.println(fruits.lastIndexOf("lemon"));
+
+        List moreFruits = List.of("cherry", "plum");
+        System.out.println(moreFruits);
     }
 }
