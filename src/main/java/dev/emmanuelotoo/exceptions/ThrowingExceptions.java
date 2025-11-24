3e900c64209f5f226793dd2df3c7d20b4438abed
Emmanuel <eotoo218@gmail.com>
2025-11-24 13:01:35 +0000

    add rethrowException and rethrowExceptions methods to ThrowingExceptions class for enhanced exception handling

--- src/main/java/dev/emmanuelotoo/exceptions/ThrowingExceptions.java
+++ src/main/java/dev/emmanuelotoo/exceptions/ThrowingExceptions.java
@@ -1,5 +1,10 @@
 package dev.emmanuelotoo.exceptions;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ThrowingExceptions {
 
     public static void main(String[] args) {
@@ -14,5 +19,18 @@ public class ThrowingExceptions {
         }
 
         return hours * payRate;
+    }
+
+    public static void rethrowException() throws IOException, InputMismatchException {
+        File file = new File("non-existent.txt");
+        file.createNewFile();
+    }
+
+
+    // Polymorphic throws statement
+    public static void rethrowExceptions() throws Exception {
+        File file = new File("non-existent.txt");
+        file.createNewFile();
+        Scanner fileReader = new Scanner(file);
     }
 }
