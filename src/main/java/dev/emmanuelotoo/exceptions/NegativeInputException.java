6385d6d2370bd80f3445383660de1fda9baa14ee
Emmanuel <eotoo218@gmail.com>
2025-11-24 12:55:14 +0000

    add NegativeInputException and ThrowingExceptions classes to handle negative input scenarios in pay calculation

---
+++ src/main/java/dev/emmanuelotoo/exceptions/NegativeInputException.java
@@ -0,0 +1,15 @@
+package dev.emmanuelotoo.exceptions;
+
+public class NegativeInputException extends Exception {
+
+
+    public NegativeInputException(){
+        this("Input must be greater than or equal to 0");
+    }
+
+
+    public NegativeInputException(String message) {
+        super(message);
+
+    }
+}
